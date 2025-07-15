7e35492b979854f9769e53e33c3ca685f9784ee4
Seth Daniel <seth@sethdaniel.org>
2025-07-15 16:38:21 -0500

    break up the notify stuff

--- lua/lsp-cd.lua
+++ lua/lsp-cd.lua
@@ -4,7 +4,7 @@ local should_change_dir = function(client, opts)
   if vim.fn.getcwd() == client.root_dir then
     return false
   end
-  if opts.ignore and vim.tbl_contains(opts.ignore or {}, client.name) then
+  if opts.ignore and vim.tbl_contains(opts.ignore, client.name) then
     return false
   elseif opts.only then
     if vim.tbl_contains(opts.only or {}, client.name) then
@@ -18,9 +18,11 @@ end
 
 M.setup = function(opts)
   opts = opts or {}
+  opts.notify = opts.notify or {}
   vim.validate('ignore', opts.ignore, 'table', true)
   vim.validate('only', opts.only, 'table', true)
-  vim.validate('notify_on_dir_change', opts.notify_on_dir_change, 'boolean', true)
+  vim.validate('notify.on_dir_change', opts.notify.on_dir_change, 'boolean', true)
+  vim.validate('notify.on_nil_root_dir', opts.notify.on_nil_root_dir, 'boolean', true)
 
   vim.api.nvim_create_autocmd({ 'LspAttach', 'BufEnter' }, {
     group = vim.api.nvim_create_augroup('lsp-cd', {}),
@@ -28,10 +30,14 @@ M.setup = function(opts)
       local clients = vim.lsp.get_clients { bufnr = o.buf }
       for _, client in pairs(clients) do
         if should_change_dir(client, opts) then
-          if opts.notify_on_dir_change then
-            vim.notify(string.format('[lsp-cd] changed directory to %s', client.root_dir), vim.log.levels.INFO)
+          if client.root_dir then
+            if opts.notify.on_dir_change then
+              vim.notify(string.format('[lsp-cd] changed directory to %s', client.root_dir), vim.log.levels.INFO)
+            end
+            vim.cmd.lcd(client.root_dir)
+          elseif opts.notify.on_nil_root_dir then
+            vim.notify(string.format '[lsp-cd] client root_dir is nil')
           end
-          vim.cmd.lcd(client.root_dir)
         end
       end
     end,
